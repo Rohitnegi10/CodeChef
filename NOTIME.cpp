2e5de1a94e8a464b97d0cdecc711b991bb36d218
Rohit Negi <rohitnegicap10@gmail.com>
2021-03-09 16:21:31 +0530

    NOTIOTIME

--- NOTIME.cpp
+++ NOTIME.cpp
@@ -1,10 +1,30 @@
 #include<iostream>
 #include<bits/stdc++.h>
 
-using namespace std
-
+using namespace std;
 
 int main()
 {
+    int N,HoursNeed,RemainingTime;
+    cin>>N>>HoursNeed>>RemainingTime;
+    int TimeZone[N];
+    for(int i =0; i<N; i++)
+    {
+        cin>>TimeZone[i];
+    }
+    int RequiredHours = HoursNeed - RemainingTime;
+
+    for(int i =0; i<N; i++)
+    {
+        if(RequiredHours <= TimeZone[i])
+            {
+            
+                cout<<"YES";
+                return 0;
+            }
+
+    }
+    cout<<"NO";
     return 0;
+
 }
